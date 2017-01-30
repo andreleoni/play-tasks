c8a078c9b16e5203c3ae574171edb16c73d3252d
André Luiz Leoni <andreluizleoni@gmail.com>
2017-01-29 23:41:48 -0200

    mark task with completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
 	before_action :set_project
+	before_action :set_task, except: [:create, :destroy]
 
 	def create		
 		@task = @project.tasks.create( task_params )
@@ -19,6 +20,15 @@ class TasksController < ApplicationController
 		redirect_to @project
 	end
 
+	def complete
+		if @task.update_attribute(:completed_at, Time.now)
+			message = "Task Completed"
+		else
+			message = "Task could not be Completed"
+		end
+		redirect_to @project, notice: message
+	end
+
 	private
 
 	def set_project
@@ -27,5 +37,9 @@ class TasksController < ApplicationController
 
 	def task_params
 		params[:task].permit(:content)
+	end
+
+	def set_task
+		@task = @project.tasks.find(params[:id])
 	end
 end
