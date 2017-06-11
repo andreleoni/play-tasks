c9d70f87621c6f366449f3bb23704b9a215a6d05
André Luiz Leoni <andreluizleoni@gmail.com>
2017-06-11 00:01:18 -0300

    all_tasks route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v0 do
-      resources :time_sheets, except: [:new, :edit]
+      get 'all_tasks' => 'tasks#all_tasks'
       
+      resources :time_sheets, except: [:new, :edit]
+
       resources :projects, except: [:new, :edit] do
         resources :tasks, except: [:new, :edit] do
           member do
